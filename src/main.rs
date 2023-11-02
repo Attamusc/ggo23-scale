c999d0640f81538e47425009598e4f6d62203115
Sean Dunn <sean@attamusc.com>
2023-11-02 09:47:59 -0400

    bones: Basic app with blank window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "ggo23-scale".into(),
+                        resolution: (1280.0, 720.0).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
+        .run();
 }
